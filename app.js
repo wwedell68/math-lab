0a173de80fd512fd346da7e46f4bc3eed6a8b61d
Wyatt Wedell <wyattearp24@aol.com>
2018-05-19 15:33:59 -0700

    finished question 4

--- app.js
+++ app.js
@@ -76,20 +76,26 @@ testSumAndMultiply(4,7,5);
 /////////////////////////////////////
 /* Problem 4
 Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
+
 IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do addition, use your sum() function that you've already created. You're going to have to be resourceful to figure out how to do this.
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
 var testArray = [2, 3, 4]; //eslint-disable-line
 
-function sumArray(sumArr) { //eslint-disable-line
+function sumArray(testArray) { //eslint-disable-line
+  var result = sumAndMultiply(testArray[0], testArray[1], testArray[2]);
+  var answer = result[0];
 
+  var message = testArray[0] + ',' + testArray[1] + ',' + testArray[2] + ' was passed in as an array of numbers, and ' + answer + ' is their sum.';
+  return [answer, message];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
