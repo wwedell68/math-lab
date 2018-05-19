2ef96c19fee943f33978414353515dbb07be1dcb
Wyatt Wedell <wyattearp24@aol.com>
2018-05-19 10:30:18 -0700

    Finished question 1

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-
+  var question1 = a + b;
+  console.log(question1);
+  var message = ('The sum of ' + a + ' and ' + b + ' is ' + question1 + '.');
+  return [question1, message];
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
